df4917f6901ffc617224e9e35038775a77972bfb
˜Willians <willians.echart@possible.com>
2021-10-10 23:56:33 -0300

    fix: route tests

--- src/routes/__tests__/routes.test.js
+++ src/routes/__tests__/routes.test.js
@@ -5,6 +5,14 @@ import { QueryClientProvider } from 'react-query';
 import { client } from '../../config/client';
 import App from '../../App';
 
+const Player = {
+  id: 1,
+  name: 'Marcos',
+  shirt: 9,
+  matches: 3,
+  goals: 4,
+};
+
 test('renders correct route at app start', () => {
   const history = createMemoryHistory();
   render(
@@ -19,6 +27,7 @@ test('renders correct route at app start', () => {
 });
 
 test('renders correct route when navigate to edit', () => {
+  localStorage.setItem('player', JSON.stringify(Player));
   const history = createMemoryHistory();
   history.push('/players/1/edit');
   render(
